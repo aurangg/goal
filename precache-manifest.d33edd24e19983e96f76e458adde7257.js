0a7b5ae6212acc0ae3becda0ace813cb18aaa90f
Aurang <aurangzebbbk@gmail.com>
2019-12-28 04:51:47 +0500

    Updates

---
+++ precache-manifest.d33edd24e19983e96f76e458adde7257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686bc530e92eb5c447eecb35a414ec8c",
+    "url": "/goal/index.html"
+  },
+  {
+    "revision": "e9abfe7cb9131009883c",
+    "url": "/goal/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "979a273d8fdedeb3a69f",
+    "url": "/goal/static/css/main.25ba9f39.chunk.css"
+  },
+  {
+    "revision": "e9abfe7cb9131009883c",
+    "url": "/goal/static/js/2.ff062f6d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goal/static/js/2.ff062f6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "979a273d8fdedeb3a69f",
+    "url": "/goal/static/js/main.a1b48050.chunk.js"
+  },
+  {
+    "revision": "f32b69cdd8b9f307e590",
+    "url": "/goal/static/js/runtime-main.9ea70191.js"
+  }
+]);
